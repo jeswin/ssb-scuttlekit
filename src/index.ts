400df33a6a8476d772abdb643c7dda127793a33c
jeswin <jeswinpk@agilehead.com>
2018-03-17 19:20:12 +0530

    Get the plugin running, vscode debug, WebSockets

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
+import * as qs from "querystring";
+import * as http from "http";
+import initSocketIO = require("socket.io");
+
 type ScuttleBot = any;
 type SSBConfig = any;
 
 function init(ssb: ScuttleBot, config: SSBConfig) {
-  
+  const port = config.scuttlekitPort || 1103;
+  const server = http.createServer((req, res) => {});
+  const io = initSocketIO(server);
+  server.listen(port, (err: any) => {
+    if (err) {
+      return console.log("Scuttlekit could not be started.", err);
+    }
+    console.log(`Scuttlekit is listening on ${port}`);
+  });
 }
-
-// exports.manifest = {
-//   read: 'source',
-//   explain: 'sync'
-// }
 
 export = {
   name: "scuttlekit",
   version: "0.0.1",
   manifest: {},
   init
-}
+};
